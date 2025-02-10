e66b68d91eaa2f13eb532a7cb190c16ce9d4ee1d
Vedbhanushali <19ce005@charusat.edu.in>
2025-02-10 23:38:13 +0530

    Add Go module and example program demonstrating slice operations

---
+++ docs/08myslices/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("slices demo")
+	var fruitsList = []string{"Apple", "Banana", "Grapes", "Orange", "Pineapple"}
+	fmt.Printf("Type of fruitsList: %T\n", fruitsList)
+	
+	fruitsList = append(fruitsList, "Mango")
+	fmt.Println(fruitsList)
+	fmt.Printf("Type of fruitsList: %T\n", fruitsList)
+	
+	// [start_index_inclusive:end_index_exclusive]
+	fruitsList = append(fruitsList[1:]) 
+	fruitsList = append(fruitsList[1:3]) 
+	fmt.Println(fruitsList)
+	fmt.Printf("Type of fruitsList: %T\n", fruitsList)
+	
+	highScores := make([]int,4) //array/slices of 4 elements memory allocated dynamically
+	highScores[0] = 234
+	highScores[1] = 945
+	highScores[2] = 465
+	highScores[3] = 867
+	// highScores[4] = 867 //will throw out of bounds error
+
+	//but using append new memory will be allocated
+	highScores = append(highScores, 555,666)
+	fmt.Println(highScores)
+	fmt.Printf("Type of highScores: %T\n", highScores)
+
+	fmt.Println("Before sorting :",highScores)
+	fmt.Println("Is sorted :",sort.IntsAreSorted(highScores))
+	fmt.Println("sorting...")
+	sort.Ints(highScores)
+	fmt.Println("Is sorted :",sort.IntsAreSorted(highScores))
+	fmt.Println("sorted :",highScores)
+
+	//how to remove certain element from slice based on index
+	var courses = []string{"Java", "Python", "Go", "C", "C++", "Ruby"}
+	fmt.Println("Before removing :",courses)
+	index := 2
+	courses = append(courses[:index], courses[index+1:]...)
+	fmt.Println("After removing :",courses)
+
+}
